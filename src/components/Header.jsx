026029fbf20075ce953422f516f59e037351cb03
Sumitsahani <sumitsahani.1855@gmail.com>
2024-11-26 13:28:50 +0530

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,14 @@ import { IoNotificationsOutline } from "react-icons/io5";
 import { IoMoonOutline } from "react-icons/io5";
 import { RxAvatar } from "react-icons/rx";
 import { IoIosArrowDown } from "react-icons/io";
+import logo from './img/immvesre_logo.svg';
 
 
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo"><img src="/img/immvesre_logo.svg" alt="Logo" /></div>
+      <div className="logo"><img src={logo} alt="Logo" /></div>
       <nav>
         <ul className="nav-links">
           <li>Home</li>
